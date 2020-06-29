44693c9d5dea947d83cd7034aae6282ee8716b5f
AlexanderBotnari <63365662+AlexanderBotnari@users.noreply.github.com>
2020-06-29 16:41:44 +0300

    Add files via upload

---
+++ src/util/ImageNoiseFilter.java
@@ -0,0 +1,25 @@
+package util;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+public class ImageNoiseFilter {
+       public ImageNoiseFilter removeNoiseFrom(String sourceFile) throws IOException {
+    	   
+    	   BufferedReader br = new BufferedReader( new FileReader(sourceFile) );
+           BufferedWriter bw = new BufferedWriter( new FileWriter(sourceFile.replace(".txt", ".clean.txt")));
+
+           int c;
+           while ((c = br.read())!=-1) {
+               if (c == 'x') c = ' ';
+               bw.write(c);
+           }
+
+           br.close();
+           bw.close();
+
+           return this;
+		
+	}
+}
